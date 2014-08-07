7c9e797882f035bc13528fb4f1411dc5b40a73ed
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-08-07 17:24:00 +0530

    call by value

---
+++ example/callbyval.cpp
@@ -0,0 +1,21 @@
+//call by value
+
+#include <iostream>
+using namespace std;
+
+void swap(int, int);	//function declaration
+
+int main(){
+	int x = 10, y = 20;
+	cout<<"Before:\tx="<<x<<"\ty="<<y<<endl;
+	swap(x, y);
+	cout<<"After:\tx="<<x<<"\ty="<<y<<endl;
+	return 0;
+}
+
+void swap(int x, int y){
+	int t = x;
+	x = y;
+	y = t;
+	cout<<"swap():\tx="<<x<<"\ty="<<y<<endl;
+}
