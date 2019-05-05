31301ee008cf60d4bd00aa731890145748d92096
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2019-05-05 14:54:24 +0530

    update

--- codesnippet/CPP.cpp
+++ codesnippet/CPP.cpp
@@ -1,6 +1,37 @@
+/**
+ * <bits/stdc++.h>
+ * is a header file that includes every standard library.
+ * use in programming contests
+ */
+// #include <bits/stdc++.h>
+
 #include <iostream>
+
 using namespace std;
-int main(){
-	
-	return 0;
+
+#define MOD  1000000007
+#define MOD9 1000000009
+
+#define fi first
+#define se second
+
+#define ui unsigned int
+#define ll unsigned long long
+#define ld long double
+
+#define testcases int testcases; cin >> testcases; while(testcases--)
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define debug(i) cout << "debug: " << i << endl
+
+#define printArr(v,n)  for(ll i = 0; i < n; i++) cout << v[i] << " "; cout << endl
+#define inputArr(v,n)  for(ll i = 0; i < n; i++) cin >> v[i]
+
+int main() {
+    fast_io
+    testcases {
+        // some code goes here...
+    }
+    return 0;
 }
